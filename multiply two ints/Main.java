b3d3c112962b3dac24bc93f171a36bc55eb2eb24
Censera <Censera@proton.me>
2025-12-07 00:43:47 +0100

    Arithmetics

---
+++ multiply two ints/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+        int x = 6,
+            y = 7;
+        int result = x * y;
+
+        System.out.println(x + " x " + y + " = " + result);
+    }
+}
---
+++ subtract two ints/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args) {
+        int x = 13,
+            y = 6;
+        int result = x - y;
+
+        System.out.println(x + " - " + y + " = " + result);
+    }
+}
+
+
+
+
